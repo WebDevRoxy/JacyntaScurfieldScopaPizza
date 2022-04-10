850c92f797dbe03de89a7fa02cf7b293ceb6cdf1
Jacynta S <62933373+WebDevRoxy@users.noreply.github.com>
2022-04-10 18:49:05 +1200

    Started remove function
    Started working on removing items from the basket

--- styles/js/mustTry.js
+++ styles/js/mustTry.js
@@ -13,6 +13,51 @@ function toggleBasket(){
 }
 
 
+//basket functionality
+
+var removeCartItemButtons = document.getElementsByClassName('delete')
+console.log(removeCartItemButtons);
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+  var button = removeCartItemButtons[i];
+  button.addEventListener('click', function(){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+  })
+}
+
+//food_price instead of cart-price
+
+//carousel_slide instead of cart-row
+
+//carousel_track_container instead of cart-items
+
+//or
+
+//price_calculation instead of cart-price
+
+//basket_item instead of cart-row
+
+//basket_container instead of cart-items
+
+//quantity_input instead of cart-quantity-input
+
+
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName('basket_container')[0];
+  var cartRows = cartItemContainer.getElementsByClassName('basket_item');
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRows = cartRows[i];
+    var priceElement = cartRows.getElementsByClassName('price_calculation')[0];
+    var quantityElement = cartRows.getElementsByClassName('quantity_input')[0];
+    console.log(priceElement, quantityElement)
+
+  }
+}
+
+
+
+
 
 /*const swiper = new Swiper('.swiper', {
   // Optional parameters
