f7586b083ec97e5909c347410e2d6ed58a46eb76
Jacynta S <62933373+WebDevRoxy@users.noreply.github.com>
2022-04-12 00:39:31 +1200

    Started on carousel slider

--- styles/js/mustTry.js
+++ styles/js/mustTry.js
@@ -190,3 +190,36 @@ function updateCartTotal(quantity) {
   //changes "total" text to total calculated value
   document.getElementById('total').innerText = total;
 }
+
+
+
+
+//carousel 
+
+const slider = document.querySelector('.casousel_track_container'),
+  slides = Array.from(document.querySelectorAll('.casousel_slide'));
+
+let isDragging = false,
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0,
+  animationID = 0,
+  currentIndex = 0
+
+slides.forEach((slide, index) => {
+  const slideImage = slide.querySelector('img')
+  slideImage.addEventListener('dragstart', (e) => e.preventDefault())
+
+  //touch events
+  slide.addEventListener('touchstart', touchStart(index))
+  slide.addEventListener('touchstart', touchEnd)
+  slide.addEventListener('touchstart', touchMove)
+
+  //mouse events
+  slide.addEventListener('mousedown', touchStart(index))
+  slide.addEventListener('mouseup', touchEnd)
+  slide.addEventListener('mouseleave', touchEnd)
+  slide.addEventListener('mousemove', touchMove)
+
+})
+
