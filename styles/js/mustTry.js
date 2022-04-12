2654a8f91d00fe0d077db7d9f1f45e1af879222a
Jacynta S <62933373+WebDevRoxy@users.noreply.github.com>
2022-04-12 13:15:15 +1200

    Javascript for carousel

--- styles/js/mustTry.js
+++ styles/js/mustTry.js
@@ -220,6 +220,67 @@ slides.forEach((slide, index) => {
   slide.addEventListener('mouseup', touchEnd)
   slide.addEventListener('mouseleave', touchEnd)
   slide.addEventListener('mousemove', touchMove)
-
 })
 
+//disable content
+window.oncontentmenu = function(event) {
+  event.preventDefault()
+  event.stopPropagation()
+  return false
+}
+
+function touchStart(index) {
+  return function(event) {
+    currentIndex = index
+    startPos = getPositionX(event)
+    isDragging = true;
+
+    animationID = requestAnimationFrame(animation)
+    slider.classList.add('grabbing')
+  }
+}
+
+function touchEnd() {
+  isDragging = false
+  cancelAnimationFrame(animatonID)
+
+  const movedBy = currentTranslate - prevTranslate
+
+  if(movedBy < -100 && currentIndex < slides.length -1) currentIndex += 1
+
+  if(movedBy > 100 && currentIndex > 0) currentIndex -= 1
+
+  setPositionByIndex()
+
+  slider.classList.remove('grabbing')
+}
+
+function touchMove(event) {
+  if(isDragging) {
+    const currentPosition = getPositionX(event)
+    currentTranslate = prevTranslate + currentPosition - startPos
+  }
+}
+
+function getPositionX(event) {
+  return event.type.includes('mouse')
+  ? event.pageX :
+  event.touches[0].clientX
+}
+
+function animation() {
+  setSliderPosition();
+  if(isDragging) requestAnimationFrame(animation)
+}
+
+function setSliderPosition() {
+  slider.style.transform = `translateX(${currentTranslate}px)
+  `
+}
+
+function setPositionByIndex() {
+  //CHECK IF INNER WINDOW IS THE CORRECT VALUE TO PUT HERE
+  currentTranslate = currentIndex * -window.innerWidth
+  prevTranslate = currentTranslate
+  setSliderPosition()
+}
