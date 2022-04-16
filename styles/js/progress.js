261d956a7de01720b273f65a1a6279f9cccac9df
Jacynta S <62933373+WebDevRoxy@users.noreply.github.com>
2022-04-16 21:51:13 +1200

    Started styling home and progress pages

--- styles/js/progress.js
+++ styles/js/progress.js
@@ -24,4 +24,4 @@ setInterval(() => {
     const computedStyle = getComputedStyle(progressBar)
     const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
     progressBar.style.setProperty('--width', width + .1)
-}, 10)
+}, 100)
